924a60c4814d385abd882ea5546c45f3fcd1cc29
crdavidson1 <callum.r.davidson@hotmail.com>
2024-03-22 13:47:07 +0000

    renamed files for convention

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import ReactDOM from 'react-dom/client';
 import React from 'react';
-import Home from './Home'
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>,
 )
--- src/renderer.ts
+++ src/renderer.ts
@@ -27,6 +27,6 @@
  */
 
 import './index.css';
-import './app';
+import './main';
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
